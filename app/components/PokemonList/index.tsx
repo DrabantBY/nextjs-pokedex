51b65fa30652eaa409395fca68155a5eb430de15
DrabantBY <drabant2022@gmail.com>
2024-11-04 15:31:45 +0300

    feat: add pokemon card component

--- app/components/PokemonList/index.tsx
+++ app/components/PokemonList/index.tsx
@@ -2,6 +2,7 @@
 import { useAppDispatch } from '@/app/lib/redux/hooks';
 import { addFavoriteAction } from '@/app/lib/redux/actions';
 import type { PokemonDetailsType } from '@/app/types/pokemonTypes';
+import PokemonCard from '../PokemonCard';
 
 type PokemonPropsType = {
 	pokemon: PokemonDetailsType[];
@@ -11,18 +12,19 @@ const PokemonList = ({ pokemon }: PokemonPropsType) => {
 	const dispatch = useAppDispatch();
 
 	return (
-		<ul>
+		<ul className="mt-6 grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 			{pokemon.map((character) => (
-				<li key={character.name}>
-					<span>{character.id}</span>
-					<button
-						type="button"
-						onClick={() => dispatch(addFavoriteAction(character))}
-					>
-						{character.name}
-					</button>
-				</li>
+				<PokemonCard key={character.id} {...character} />
 			))}
+			{/*  <li key={character.name}>
+				 	<span>{character.id}</span>
+				 	<button
+				 		type="button"
+				 		onClick={() => dispatch(addFavoriteAction(character))}
+				 	>
+				 		{character.name}
+				 	</button>
+				 </li> */}
 		</ul>
 	);
 };
