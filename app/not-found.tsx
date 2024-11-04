38ad0a4454bc11b3a1a9f7b8c18f45d30f16db7b
DrabantBY <drabant2022@gmail.com>
2024-11-04 22:00:22 +0300

    feat: add error page component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,21 +1,7 @@
-import NavLink from './ui/NavLink';
+import ErrorPage from './ui/ErrorPage';
 
 const NotFound = () => {
-	return (
-		<section className="flex flex-col grow justify-center items-center text-center">
-			<h6 className="mb-4 text-lg font-semibold text-gray-500">404</h6>
-
-			<h2 className="mb-6 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
-				Page not found
-			</h2>
-
-			<p className="mb-10 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
-				Sorry, we couldn’t find the page you’re looking for.
-			</p>
-
-			<NavLink route="/" label="go back" active={false} />
-		</section>
-	);
+	return <ErrorPage />;
 };
 
 export default NotFound;
