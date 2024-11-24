51cbc66c1bc01c7068733d856944bb4f951d6f72
DrabantBY <drabant2022@gmail.com>
2024-11-24 13:33:10 +0300

    refactor: add useDeferredValue

--- ui/Pokemon.tsx
+++ ui/Pokemon.tsx
@@ -5,11 +5,14 @@ import Nothing from '@/ui/Nothing';
 import Spinner from '@/ui/Spinner';
 import useScroll from '@/lib/hooks/useScroll';
 import useFavorite from '@/lib/hooks/useFavorite';
+import { useDeferredValue } from 'react';
 import { useAppSelector } from '@/lib/redux/hooks';
 import { selectPokemon, selectNext } from '@/lib/redux/selectors';
 
 const Pokemon = () => {
 	const pokemon = useAppSelector(selectPokemon);
+
+	const deferredPokemon = useDeferredValue(pokemon);
 
 	const next = useAppSelector(selectNext);
 
@@ -21,7 +24,7 @@ const Pokemon = () => {
 		<>
 			{pokemon.length === 0 && !next ? <Nothing /> : null}
 			{pokemon.length > 0 ? (
-				<PokemonList pokemon={pokemon} toggle={setFavoritePokemon} />
+				<PokemonList pokemon={deferredPokemon} toggle={setFavoritePokemon} />
 			) : null}
 			{next ? (
 				<div ref={ref} className="mb-4 flex grow items-center">
